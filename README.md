d06212699504d0bf1c5296f5766348f26918ea09
Chris <chris@contolini.com>
2012-09-30 16:38:30 +0300

    all sorts of documentation improvements

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Rolling Jubilee is a network of debtors who liberate debtors through mutual aid.
 
 ## License
 
-Copyright (c) 2012 Chris Contolini <chris@contolini.com>
+Copyright (c) 2012 Rolling Jubilee
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- assets/js/rj.js
+++ assets/js/rj.js
@@ -1,32 +1,32 @@
 /**
  * Namespace the main app
- * 
+ *
  */
 var RJ = RJ || {
-  
+
   /**
    * Key of the spreadsheet we're pulling data from
-   * 
+   *
    */
   SPREADSHEETKEY: '0Ao7re1ITFPKydFhKcGFDT2JpTnphbnNubTUwbThVSEE',
-  
+
   /**
    * Create a new Miso dataset and load a Google spreadsheet into it
-   * 
+   * http://misoproject.com/dataset/tutorials/googlespreadsheets
    */
   loadData: function (name, worksheetId, columns) {
 
     var data = [],
         content = {};
-  
+
     var ds = new Miso.Dataset({
       importer: Miso.Importers.GoogleSpreadsheet,
       parser: Miso.Parsers.GoogleSpreadsheet,
       key: RJ.SPREADSHEETKEY,
       worksheet: worksheetId
     });
-      
-    ds.fetch({ 
+
+    ds.fetch({
       success : function() {
         this.each(function(row) {
           data.push(row);
@@ -35,66 +35,67 @@ var RJ = RJ || {
         RJ.templatize(name, content, columns);
       },
       error : function() {
-        console.log("Are you sure you are connected to the internet?");
+        console.log("Error loading Google spreadsheet.");
       }
     });
-      
+
   },
-  
+
   /**
    * Feed data into a Handlebars template
-   * 
+   * http://handlebarsjs.com
    */
   templatize: function (name, data, columns) {
-  
+
+    // if it's a video we're dealing with, extract the video id to grab a thumbnail
     if (name === 'videos') {
       _.each(data.videos, function(num, key){
         var video_id = data.videos[key].video_url.split('v=')[1],
             ampersandPosition = video_id.indexOf('&');
-        if(ampersandPosition != -1) {
+        if(ampersandPosition !== -1) {
           video_id = video_id.substring(0, ampersandPosition);
         }
         data.videos[key].thumbnail = 'https://i3.ytimg.com/vi/' + video_id + '/0.jpg';
       });
     }
-    
+
     // if columns have been specified, transform the array
     if (columns !== undefined) {
       data = RJ.transform(data[name], columns);
     }
-    
+
     var source = $('#' + name + '-template').html(),
         template = Handlebars.compile(source);
     $('#' + name + '-container').append(template(data));
-    
+
   },
-  
+
   /**
-   * Splice an array into a specified number of columns
-   * 
+   * Utility function to splice an array into a specified number of columns
+   *
    */
   transform: function (arr, num) {
-  
+
     var result = [], temp = [];
     _.each(arr, function ( elem, i ) {
-        if ( i > 0 && i % num === 0 ) {
+        if (i > 0 && i % num === 0) {
             result.push( temp );
             temp = [];
         }
         temp.push( elem );
     });
-    if ( temp.length > 0 ) {
-        result.push( temp );
+    if (temp.length > 0) {
+        result.push(temp);
     }
     return result;
-  
+
   }
-  
+
 };
 
 /**
  * When DOM is loaded...
- * 
+ *
  */
 $(function(){
 
@@ -104,7 +105,7 @@ $(function(){
   RJ.loadData('social-media', '4');
   RJ.loadData('faq1', '5');
   RJ.loadData('faq2', '6');
-  
+
   // fancy box modals
   $('.fancybox-media').fancybox({
 		openEffect: 'none',
@@ -113,7 +114,7 @@ $(function(){
 			media: {}
 		}
 	});
-	
+
   // scroll when nav items are clicked
   $("nav a").click(function(){
     var el = $(this).attr('href'),
@@ -123,5 +124,5 @@ $(function(){
       }, 700);
     return false;
   });
-  
+
 });

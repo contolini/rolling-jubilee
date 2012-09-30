6f0b6fa4ea14d024fce3ccd14a8914f5b58a75e1
Chris <chris@contolini.com>
2012-09-30 18:52:38 +0300

    added counter

---
+++ README.md
@@ -0,0 +1,16 @@
+Rolling Jubilee
+===============
+
+Website for Rolling Jubilee. Uses [Google Drive API](https://developers.google.com/drive/) via [Miso](http://misoproject.com/) to pull content from a [spreadsheet](https://docs.google.com/spreadsheet/ccc?key=0Ao7re1ITFPKydFhKcGFDT2JpTnphbnNubTUwbThVSEE) and populate [Handlebars](http://handlebarsjs.com/) templates. Fully-responsive layout based on [Bootstrap](twitter.github.com/bootstrap/).
+
+Rolling Jubilee is a network of debtors who liberate debtors through mutual aid. We legally buy up debt for pennies on the dollar. then instead of collecting it, we abolish it.
+
+## License
+
+Copyright (c) 2012 Rolling Jubilee
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/js/rj.js
+++ assets/js/rj.js
@@ -1,32 +1,32 @@
 /**
  * Namespace the main app
- * 
+ *
  */
 var RJ = RJ || {
-  
+
   /**
    * Key of the spreadsheet we're pulling data from
-   * 
+   *
    */
   SPREADSHEETKEY: '0Ao7re1ITFPKydFhKcGFDT2JpTnphbnNubTUwbThVSEE',
-  
+
   /**
    * Create a new Miso dataset and load a Google spreadsheet into it
-   * 
+   * http://misoproject.com/dataset/tutorials/googlespreadsheets
    */
   loadData: function (name, worksheetId, columns) {
 
     var data = [],
         content = {};
-  
+
     var ds = new Miso.Dataset({
       importer: Miso.Importers.GoogleSpreadsheet,
       parser: Miso.Parsers.GoogleSpreadsheet,
       key: RJ.SPREADSHEETKEY,
       worksheet: worksheetId
     });
-      
-    ds.fetch({ 
+
+    ds.fetch({
       success : function() {
         this.each(function(row) {
           data.push(row);
@@ -35,76 +35,78 @@ var RJ = RJ || {
         RJ.templatize(name, content, columns);
       },
       error : function() {
-        console.log("Are you sure you are connected to the internet?");
+        console.log("Error loading Google spreadsheet.");
       }
     });
-      
+
   },
-  
+
   /**
    * Feed data into a Handlebars template
-   * 
+   * http://handlebarsjs.com
    */
   templatize: function (name, data, columns) {
-  
+
+    // if it's a video we're dealing with, extract the video id to grab a thumbnail
     if (name === 'videos') {
       _.each(data.videos, function(num, key){
         var video_id = data.videos[key].video_url.split('v=')[1],
             ampersandPosition = video_id.indexOf('&');
-        if(ampersandPosition != -1) {
+        if(ampersandPosition !== -1) {
           video_id = video_id.substring(0, ampersandPosition);
         }
         data.videos[key].thumbnail = 'https://i3.ytimg.com/vi/' + video_id + '/0.jpg';
       });
     }
-    
+
     // if columns have been specified, transform the array
     if (columns !== undefined) {
       data = RJ.transform(data[name], columns);
     }
-    
+
     var source = $('#' + name + '-template').html(),
         template = Handlebars.compile(source);
     $('#' + name + '-container').append(template(data));
-    
+
   },
-  
+
   /**
-   * Splice an array into a specified number of columns
-   * 
+   * Utility function to splice an array into a specified number of columns
+   *
    */
   transform: function (arr, num) {
-  
+
     var result = [], temp = [];
     _.each(arr, function ( elem, i ) {
-        if ( i > 0 && i % num === 0 ) {
+        if (i > 0 && i % num === 0) {
             result.push( temp );
             temp = [];
         }
         temp.push( elem );
     });
-    if ( temp.length > 0 ) {
-        result.push( temp );
+    if (temp.length > 0) {
+        result.push(temp);
     }
     return result;
-  
+
   }
-  
+
 };
 
 /**
  * When DOM is loaded...
- * 
+ *
  */
 $(function(){
 
+  // load all the different worksheets
   RJ.loadData('videos', '1', 4);
   RJ.loadData('allies', '2');
   RJ.loadData('knowledge', '3');
   RJ.loadData('social-media', '4');
   RJ.loadData('faq1', '5');
   RJ.loadData('faq2', '6');
-  
+
   // fancy box modals
   $('.fancybox-media').fancybox({
 		openEffect: 'none',
@@ -113,9 +115,9 @@ $(function(){
 			media: {}
 		}
 	});
-	
+
   // scroll when nav items are clicked
-  $("nav a").click(function(){
+  $("nav.main a").click(function(){
     var el = $(this).attr('href'),
         loc = $(el).offset().top - 20;
     $("html,body").animate({
@@ -123,5 +125,19 @@ $(function(){
       }, 700);
     return false;
   });
-  
+
+  // jquery odometer
+  $('.counter').jOdometer({
+    increment: 0,
+    counterStart:'1',
+    numbersImage: '/assets/img/jodometer-numbers-24pt.png',
+    widthNumber: 32,
+    heightNumber: 54,
+    spaceNumbers: 0,
+    offsetRight:-10,
+    maxDigits: 5,
+    prefixChar: true,
+    delayTime: 0
+  });
+
 });
